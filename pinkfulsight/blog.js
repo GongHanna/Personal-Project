edd65fe307cb9afd63005e856249f42d327c96e8
GongHanna <han_nah2853@naver.com>
2024-07-20 17:44:56 +0900

    서브페이지 js 이벤트 수정

--- pinkfulsight/blog.js
+++ pinkfulsight/blog.js
@@ -3,6 +3,12 @@ $('a[href="#"]').on("click", (e) => {
   e.preventDefault();
 });
 
+/* 모바일 버튼 클릭 시 메뉴 보이기 */
+$(".header .mobile-btn").on("click", () => {
+  $(".header .mobile-btn").toggleClass("toggle");
+  $(".header .global-menu").slideToggle();
+});
+
 /* 메뉴 클릭 시 페이지 이동 */
 $(".header .global-menu li a").each(function (idx, elem) {
   $(elem).on("click", (e) => {
