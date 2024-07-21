58910c220a3a2f1d9ae10882fbaee8a4d7e560bb
GongHanna <han_nah2853@naver.com>
2024-07-21 12:17:12 +0900

    js 수정

--- pinkfulsight/blog.js
+++ pinkfulsight/blog.js
@@ -8,29 +8,3 @@ $(".header .mobile-btn").on("click", () => {
   $(".header .mobile-btn").toggleClass("toggle");
   $(".header .global-menu").slideToggle();
 });
-
-/* 메뉴 클릭 시 페이지 이동 */
-$(".header .global-menu li a").each(function (idx, elem) {
-  $(elem).on("click", (e) => {
-    e.preventDefault();
-
-    const targetId = $(elem).attr('href');
-    const contentsHt = $(targetId).offset().top;
-    scrollTo({
-      top: contentsHt,
-      behavior: "smooth",
-    });
-  });
-});
-$(".footer .footer-menu li a").each(function (idx, elem) {
-  $(elem).on("click", (e) => {
-    e.preventDefault();
-
-    const targetId = $(elem).attr('href');
-    const contentsHt = $(targetId).offset().top;
-    scrollTo({
-      top: contentsHt,
-      behavior: "smooth",
-    });
-  });
-});
